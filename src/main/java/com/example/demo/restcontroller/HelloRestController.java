37844ec91768dc961e5f7ba33c1de51e877425b4
Sunny <shanni_chen@tej.com.tw>
2022-09-26 16:26:33 +0800

    set properties

--- src/main/java/com/example/demo/restcontroller/HelloRestController.java
+++ src/main/java/com/example/demo/restcontroller/HelloRestController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloRestController {
 
 	@RequestMapping("/hello")
+	@CrossOrigin
 	public ResponseEntity<String> hello(){
 		
 			
