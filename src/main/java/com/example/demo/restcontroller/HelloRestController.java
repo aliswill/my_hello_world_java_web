4bbd6ef61623b2080fb3f2ebf6a62dac75ad4c5c
Sunny <shanni_chen@tej.com.tw>
2022-09-28 10:32:19 +0800

    test return hello3

--- src/main/java/com/example/demo/restcontroller/HelloRestController.java
+++ src/main/java/com/example/demo/restcontroller/HelloRestController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,11 +16,21 @@ public class HelloRestController {
 
 	@RequestMapping("/hello")
 	@CrossOrigin
-	public ResponseEntity<String> hello(){
-		
-			
+	public ResponseEntity<String> hello(){					
 		return new ResponseEntity<>("OK，我說了你好", HttpStatus.OK);
 	}
+	
+	@RequestMapping("/hello2")
+	public String hello2(){					
+		return "hello222";//回傳文字到頁面上?
+	}
+	
+	@RequestMapping("/hello3")
+	//@ResponseBody
+	public String hello3(){					
+		return "hello3";//
+	}
+
 
 	
 }
