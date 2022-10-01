882cc1f99f87cfc4d2e0d2ddc2bf0414f28dd024
Sunny <shanni_chen@tej.com.tw>
2022-10-01 21:17:45 +0800

    add sunnyhome

--- src/main/java/com/example/demo/restcontroller/HelloRestController.java
+++ src/main/java/com/example/demo/restcontroller/HelloRestController.java
@@ -4,13 +4,15 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//@Controller回傳view頁面 RestController回傳json等，
+@RestController //RestController，他就是Controller 加 @ResponseBody 的註解，也相當在裡面的方法都標了這兩個註解
 @RequestMapping("/api")
 public class HelloRestController {
 
@@ -21,12 +23,12 @@ public class HelloRestController {
 	}
 	
 	@RequestMapping("/hello2")
+	@ResponseBody
 	public String hello2(){					
 		return "hello222";//回傳文字到頁面上?
 	}
 	
 	@RequestMapping("/hello3")
-	//@ResponseBody
 	public String hello3(){					
 		return "hello3";//
 	}
