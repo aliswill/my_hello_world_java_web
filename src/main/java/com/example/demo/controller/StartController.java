49e7f9948ad8cf37591d2240670c35ec29e0bdf4
2019111101 <2019111101@FINPC-250.tejwin.com>
2022-10-01 21:33:15 +0800

    補SunnyHome

--- src/main/java/com/example/demo/controller/StartController.java
+++ src/main/java/com/example/demo/controller/StartController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class StartController {
 
-	@RequestMapping("/")
+	@RequestMapping("/sunnyhome")
 	public String getHone() {
 		return "sunny/sunnyhome";
 	}
