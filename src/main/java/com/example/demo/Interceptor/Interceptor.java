820bfc67dda99da2e0517a41d70f34a2680b1537
Sunny <shanni_chen@tej.com.tw>
2022-10-06 17:50:13 +0800

    重推攔截器

---
+++ src/main/java/com/example/demo/Interceptor/Interceptor.java
@@ -0,0 +1,39 @@
+package com.example.demo.Interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+//檢查是否有登入
+//攔截器 寫完需要配置=>去哪配置?
+
+@Component
+public class Interceptor implements HandlerInterceptor {
+
+	@Override
+	//目標方法執行之前
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		// TODO Auto-generated method stub
+		Object loginuser = request.getSession().getAttribute("loginUser");
+		System.out.println("dsf");
+		System.out.println(request.getSession());
+		//未登入，回到登入頁面
+		if(loginuser==null) {
+			request.setAttribute("msg", "使用者尚未登入");
+			request.getRequestDispatcher("/sunnyhome").forward(request, response);
+			return false;
+		}
+		//已登入
+		else {
+			request.setAttribute("msg", "使用者已經登入");//DEBUG
+			return true;//放行請求
+		}
+		//return HandlerInterceptor.super.preHandle(request, response, handler);
+	}
+
+
+
+}
---
+++ src/main/java/com/example/demo/myConfig/MyConfigureration.java
@@ -0,0 +1,21 @@
+package com.example.demo.myConfig;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.example.demo.Interceptor.Interceptor;
+
+@Configuration
+public class MyConfigureration implements WebMvcConfigurer {
+
+	//配置攔截器
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		// TODO Auto-generated method stub
+		//頁面路徑跟靜態資原路徑都是解析的那個，不管是用thymeleaf解析或原使靜態資原路徑
+		registry.addInterceptor(new Interceptor()).addPathPatterns("/**").excludePathPatterns("/","/sunnyhome","css/**","javascript/**");
+		
+	}
+
+}
