fa31d345d78b135b971a174be3c2c52e7125c343
Sunny <shanni_chen@tej.com.tw>
2022-10-06 17:17:24 +0800

    攔截器

--- src/main/java/com/example/demo/controller/StartController.java
+++ src/main/java/com/example/demo/controller/StartController.java
@@ -3,8 +3,11 @@ package com.example.demo.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -12,17 +15,36 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class StartController {
 
 	@RequestMapping("/sunnyhome")
-	public String getHone() {
+	public String getHome() {
 		return "sunny/sunnyhome";
 	}
 	
-	@RequestMapping("/afterlogin")
-	public String login(@RequestParam("useraccount") String useraccount,@RequestParam("userpassword") String userpassword,Map<String,String> map) {
-		//Map<String,String> map = new HashMap<>();
-		map.put("useraccount", useraccount);
-		map.put("userpassword", userpassword);
+	//為了給重定向到登入後的頁面而建的方法
+	@RequestMapping("/ToLogin")
+	public String getToLogin() {
 		return "sunny/login";
+	}
+	
+	@PostMapping("/afterlogin")
+	public String login(@RequestParam("useraccount") String useraccount,@RequestParam("userpassword") String userpassword,Map<String,String> map,HttpSession session) {
+		//Map<String,String> map = new HashMap<>();
+		if(useraccount.equals("sunny")&& userpassword!=null) {
+			session.setAttribute("loginUser", useraccount);
+			map.put("useraccount", useraccount);
+			map.put("userpassword", userpassword);
+			return "sunny/login";
+			//return "/afterlogin";
+			//return "redirect:/ToLogin";//登入成功需防止使用者刷新頁面，使表單重複提交，需要重定向，但會丟失傳輸資訊
+		}
+		else {
+			map.put("er_msg", "登入失敗");
+			return "sunny/sunnyhome";
+		}
+		
+
 	}
 
 	
 }
+
+
