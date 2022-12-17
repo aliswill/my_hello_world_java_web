3c63c52ba78a48e20fa5f4448be050814cf5e4c6
Sunny <shanni_chen@tej.com.tw>
2022-12-18 00:43:13 +0800

    檔案管理完成

--- src/main/java/com/example/demo/controller/FileController.java
+++ src/main/java/com/example/demo/controller/FileController.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import javax.servlet.ServletContext;
@@ -29,9 +31,40 @@ import org.springframework.web.multipart.MultipartFile;
 @Controller
 public class FileController {
 
+	private String upload_path = "D:\\hello_world_upload";
+	
 	@GetMapping("/file_control")
 	public String file() {
 		return "file_control";
+	}
+	
+	@ResponseBody
+	@GetMapping("/getUserFiles")//載入此頁面時，由ajax發請求 回傳該帳戶使用者的資料夾中的所有檔按
+	public ResponseEntity<Object> getUserFiles(HttpSession session){
+		String useraccount = (String) session.getAttribute("loginUser");
+		File userfolder = new File(upload_path+"\\"+useraccount);
+		if(!userfolder.exists()) {
+			return null;
+		}
+		String[] files = userfolder.list();
+		return new ResponseEntity<>(files, HttpStatus.OK);
+	}
+	
+	
+	@PostMapping("/deleteFiles") //此方法接收複選框結果 //@RequestBody String[] del_files報錯，注意參數接收 //@RequestParam Map<String,Object>
+	public String deleteFiles(@RequestParam List<String> del_files,HttpSession session) {
+		String useraccount = (String) session.getAttribute("loginUser");
+		//這兩種參數可以:@RequestParam String[] 
+		//@RequestParam Map<String,Object>接不到全部值 、@RequestBody String[]不行
+
+		for(String del_file:del_files) {
+			File file = new File(upload_path+"\\"+useraccount+"\\"+del_file);
+			file.delete();
+			System.out.println("你刪除了"+del_file);
+		}
+		
+		//del_files=1.txt & del_files=2.txt & del_files=3.pptx
+		return "redirect:/file_control";
 	}
 	
 	@ResponseBody
@@ -60,24 +93,28 @@ public class FileController {
 
 	}
 	
-	@PostMapping("/uploadFile")
-	public String uploadFile(@RequestParam MultipartFile uploadfile,HttpSession session) throws IllegalStateException, IOException {
-		String uploadpath ="D:\\hello_world_upload";
+	@PostMapping("/uploadFile")//每個會員有各自專屬資料夾
+	public String uploadFile(@RequestParam MultipartFile[] uploadfiles,HttpSession session) throws IllegalStateException, IOException {
+		String useraccount = (String) session.getAttribute("loginUser");
+		String uploadpath ="D:\\hello_world_upload\\"+useraccount;
 		File dir = new File(uploadpath);
 		if(!dir.exists()) {
 			dir.mkdir();
 		}	
-		String filename = uploadfile.getOriginalFilename();
-		String filepreffix = filename.substring(0,filename.lastIndexOf("."));
-		String filesuffix = filename.substring(filename.lastIndexOf("."));
-		//uploadfile.transferTo(new File(dir.getAbsolutePath()+File.separator+uploadfile.getOriginalFilename()));
-		//String finalfilename = UUID.randomUUID().toString()+filesuffix;
-		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
-		String finalfilename = filepreffix+dtf.format(LocalDateTime.now())+filesuffix;
-		uploadfile.transferTo(new File(dir.getAbsolutePath()+File.separator+finalfilename));
-		System.out.println("上傳成功，將文件上傳至"+dir.getAbsolutePath()+File.separator+finalfilename);
-		session.setAttribute("uploadmsg","上傳成功!您最後一次上傳的檔案為："+filename);
-		//uploadmsg.addAttribute("uploadmsg","上傳成功!您最後一次上傳的檔案為："+filename);
-		return "file_control";
+		
+		for(MultipartFile uploadfile:uploadfiles) {
+			String filename = uploadfile.getOriginalFilename();
+			String filepreffix = filename.substring(0,filename.lastIndexOf("."));
+			String filesuffix = filename.substring(filename.lastIndexOf("."));
+			//uploadfile.transferTo(new File(dir.getAbsolutePath()+File.separator+uploadfile.getOriginalFilename()));
+			//String finalfilename = UUID.randomUUID().toString()+filesuffix;
+			DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+			String finalfilename = filepreffix+dtf.format(LocalDateTime.now())+filesuffix;
+			uploadfile.transferTo(new File(dir.getAbsolutePath()+File.separator+finalfilename));
+			System.out.println("上傳成功，將文件上傳至"+dir.getAbsolutePath()+File.separator+finalfilename);
+			session.setAttribute("uploadmsg","上傳成功!您最後一次上傳的檔案為："+filename);
+
+		}				
+		return "redirect:/file_control";
 	}
 }
--- src/main/resources/static/javascript/file.js
+++ src/main/resources/static/javascript/file.js
@@ -3,6 +3,42 @@
  */
  
  $(document).ready(function(){
+	$.ajax({
+			url:"/getUserFiles",
+			type:"GET",			
+			success:function(files){				
+				$(files).each(function(){
+					var file = this;
+					
+					//$("#test").append("<p>"+file+"</p>");
+					//提交後會是提交鍵值對 name=value 所以name不能用成檔案名，所以複選框的提交結果會是 name=value1&value2
+					$("#files_form_ckeckbox").append('<input type="checkbox" name="del_files"' +'value="'+file+'"'+'id="'+file+'"/>');
+					$("#files_form_ckeckbox").append('<label for="'+file+'">'+file+'</label></br>');
+				})
+					//$("#files_form").append('<input type="submit" id="butten" value="刪除">');
+			}			
+		});
+		
+	$("#butten").click(function(){
+		//***如果按鈕也是由ajax新增的，似乎無法操作?
+		//$(this).parent("form")["del_files"].value==""
+		
+		var check=$("input[name='del_files']:checked").length;//判斷有多少個方框被勾選
+			if(check==0){
+				alert("您尚未勾選任何項目");
+				return false;//不要提交表單
+			}else{
+				var del_yn = confirm("確定刪除檔案?");
+				if(del_yn){
+					return true;
+				}else{
+					return false;
+				}
+			}
+
+	});
+	
+
 /*	$("#download_txt").click(function(){
 		$.ajax({
 			url:"/downloadTxt",
